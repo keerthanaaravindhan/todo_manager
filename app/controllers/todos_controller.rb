bc9aeba6cd7bdfee950b34a8d5006fbf8140e5e4
keerthanaaravindhan <keerthana.aravindhan31@gmail.com>
2020-03-09 00:34:05 +0530

    can see a single todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,11 @@
 # todos_controller.rb
 class TodosController < ApplicationController
-    def index
-      render plain: Todo.all.to_a
-    end
+  def index
+    render plain: Todo.all.order(:id).map{|todo| todo.looks}.join("\n")
   end
+  def show
+    id=params[:id]
+    todo=Todo.find(id)
+    render plain: todo.looks
+  end
+end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,7 @@
 class Todo < ActiveRecord::Base
-
+    def looks
+        is_completed= completed ? "[X]" : "[ ]"
+        "#{id}  #{todo_text}  #{due_date} #{is_completed}"
+    end
 end
+ 
