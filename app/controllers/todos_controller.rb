32f5305b5f3000e7332235e6e416e8b7759388ff
keerthanaaravindhan <keerthana.aravindhan31@gmail.com>
2020-03-26 23:15:00 +0530

    New changes are made

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,25 +1,30 @@
 # todos_controller.rb
 class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
+
   def index
-    render plain: Todo.all.order(:id).map{|todo| todo.looks}.join("\n")
+    # render plain: Todo.all.order(:id).map{|todo| todo.looks}.join("\n")
+    render "index"
   end
+
   def show
-    id=params[:id]
-    todo=Todo.find(id)
+    id = params[:id]
+    todo = Todo.find(id)
     render plain: todo.looks
   end
+
   def create
-    todo_txt=params[:todo_text]
-    due_date=DateTime.parse(params[:due_date])
-    new_todo=Todo.create!(todo_text:todo_txt,due_date:due_date,completed:false)
+    todo_txt = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(todo_text: todo_txt, due_date: due_date, completed: false)
     render plain: "New todo is created at id #{new_todo.id}"
-  end 
+  end
+
   def update
-    id=params[:id]
-    completed=params[:completed]
-    todo=Todo.find(id)
-    todo.completed=completed
+    id = params[:id]
+    completed = params[:completed]
+    todo = Todo.find(id)
+    todo.completed = completed
     todo.save!
     render plain: todo.looks
   end
