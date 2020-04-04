fc77e52eac3a81c43cbefd07ecc389483ace6159
keerthanaaravindhan <keerthana.aravindhan31@gmail.com>
2020-04-04 21:19:08 +0530

    Added delete option

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -31,4 +31,11 @@ class TodosController < ApplicationController
     #render plain: todo.looks
     redirect_to todos_path
   end
+
+  def destroy
+    id= params[:id]
+    todo=Todo.find(id)
+    todo.destroy
+    redirect_to todos_path
+  end
 end
