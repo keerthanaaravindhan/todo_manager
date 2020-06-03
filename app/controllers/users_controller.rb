cb1ef9d3ef017da30931408c3f56f414d71bc010
keerthanaaravindhan <keerthana.aravindhan31@gmail.com>
2020-06-03 19:19:52 +0530

    Adding level 9 target

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,16 +19,32 @@ class UsersController < ApplicationController
   #end
 
   # this create is used when sign up is used
+  #def create
+   # User.create!(
+    #  first_name: params[:first_name],
+     # last_name: params[:last_name],
+     # email: params[:email],
+     # password: params[:password],
+    #)
+    #redirect_to "/"
+  #end
+
   def create
-    User.create!(
+    new_user = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password],
     )
-    redirect_to "/"
+    if new_user.save
+      flash[:error] = "User Registered. Sign-in to continue"
+      redirect_to root_path
+    else
+      flash[:error] = new_user.errors.full_messages.join(", ")
+      redirect_to new_user_path
+    end
   end
-
+  
   def login
     email = params[:email]
     pwd = params[:password]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
-class User < ApplicationRecord
+#class User < ApplicationRecord
+#  has_secure_password
+#  has_many :todos                                                                                                                                                                                                                                                                                                                                                                                                                   
+#  def looks
+#    "#{id}  #{name}  #{email} "
+#  end
+#end
+
+class User < ActiveRecord::Base
+
+  validates :first_name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
   has_secure_password
-  has_many :todos                                                                                                                                                                                                                                                                                                                                                                                                                   
-  def looks
-    "#{id}  #{name}  #{email} "
-  end
+  has_many :todos
+  
 end
